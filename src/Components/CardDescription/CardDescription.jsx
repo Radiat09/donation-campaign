93e6c4a1eb503689488fad856366c9f375e4d1cf
Radiat09 <rzsianboyridoy10@gmail.com>
2023-09-25 01:23:09 +0600

    finished card Details component designing with react toast

--- src/Components/CardDescription/CardDescription.jsx
+++ src/Components/CardDescription/CardDescription.jsx
@@ -1,24 +1,41 @@
 import { useLoaderData, useParams } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const CardDescription = () => {
   const cards = useLoaderData();
   const { id } = useParams();
   const card = cards.find((card) => card.id == id);
   console.log(id, cards, card);
-  const { price, description, title, image } = card;
+  const { price, description, title, image, text_color } = card;
+
+  const handleDonatebtn = () => {
+    toast.success("Congratulation,You have successfully Donated");
+  };
   return (
-    <div className="max-w-7xl mx-auto">
-      <figure className="flex justify-center items-center">
-        <img className="w-4/5" src={image} alt="" />
-      </figure>
-      <button>
-        Donate <span>${price}</span>
-      </button>
-      <div>
-        <h3>{title}</h3>
-        <p>{description}</p>
+    <>
+      <div className="max-w-7xl mx-auto my-20">
+        <section>
+          <div className="relative">
+            <figure className="">
+              <img className="w-full h-3/4 rounded-lg" src={image} alt="" />
+            </figure>
+            <button
+              onClick={handleDonatebtn}
+              className="text-white px-7 py-4 rounded text-xl font-semibold absolute bottom-7 left-8"
+              style={{
+                backgroundColor: `${text_color}`,
+              }}
+            >
+              Donate <span>${price}</span>
+            </button>
+          </div>
+          <h3 className="text-4xl font-bold mb-6 mt-14">{title}</h3>
+          <p className="font-normal">{description}</p>
+        </section>
       </div>
-    </div>
+      <ToastContainer />
+    </>
   );
 };
 
--- src/NavBar/NavBar.jsx
+++ src/NavBar/NavBar.jsx
@@ -3,14 +3,14 @@ import { NavLink } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav>
-      <div className="navbar bg-base-100 max-w-7xl mx-auto">
+      <div className="navbar  max-w-7xl mx-auto">
         <div className="flex-1">
           <NavLink>
             <img src="/public/Logo.png" alt="Logo" />
           </NavLink>
         </div>
         <div className="flex-none">
-          <ul className="menu menu-horizontal px-1">
+          <ul className="menu menu-horizontal px-1 text-lg font-medium">
             <li className="mr-2">
               <NavLink to="/">Home</NavLink>
             </li>
