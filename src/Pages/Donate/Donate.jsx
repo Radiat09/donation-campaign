4ae8d27c864b7353abe33efc6885db632d8c9829
Radiat09 <rzsianboyridoy10@gmail.com>
2023-09-24 22:24:11 +0600

    added pages folder

---
+++ src/Pages/Donate/Donate.jsx
@@ -0,0 +1,9 @@
+const Donate = () => {
+  return (
+    <div>
+      <h1 className="text-6xl text-center"> This is Donate Page</h1>
+    </div>
+  );
+};
+
+export default Donate;
---
+++ src/Pages/Statistics/Statistics.jsx
@@ -0,0 +1,9 @@
+const Statistics = () => {
+  return (
+    <div>
+      <h1 className="text-6xl text-center">This is Statictics page</h1>
+    </div>
+  );
+};
+
+export default Statistics;
